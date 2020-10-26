2b92e34e5f322b3f180ba02d9da0c1f456a95c7d
Florian Goossens <floriangoossens@air-van-florian.home>
2020-10-26 15:32:06 +0100

    aanmaken van project

---
+++ Project/Project/Controllers/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project
+//
+//  Created by Florian Goossens on 23/10/2020.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
