54783ca8c8a487049ff8a8b86cb8d5386eaa0448
Florian Goossens <floriangoossens@MacBook-Air-van-Florian.local>
2020-10-26 17:36:09 +0100

    UI en implentatie van HomeVC - BeersVC, CocktailsVC, FavoritsVC, RatingsVC en basis navigatie toegevoed

--- Project/Project/Controllers/HomeViewController.swift
+++ Project/Project/Controllers/HomeViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    //OUTLETS
+    @IBOutlet var beersButton: UIButton!
+    @IBOutlet var cocktailsButton: UIButton!
+    @IBOutlet var favoritsButton: UIButton!
+    @IBOutlet var ratingsButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
 
-
+    @IBAction func navigateToNextScreen(_ sender: UIButton) {
+        
+        guard let buttonTitle = sender.currentTitle else {
+            return
+        }
+        performSegue(withIdentifier: buttonTitle, sender: nil)
+    }
+    
 }
 
